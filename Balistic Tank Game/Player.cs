8cede302f84462a6c8ce40dfddac537eac8c840d
Karlos <90242762+Karlos-12@users.noreply.github.com>
2023-09-28 19:29:34 +0200

    Přidejte soubory projektu.

---
+++ Balistic Tank Game/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balistic_Tank_Game
+{
+    class Player
+    {
+        public string username { get; internal set; }
+        public int ammo {  get; internal set; }
+        public int health { get; internal set; }
+        public int armor { get; internal set; }
+        public Tank tank { get; internal set; }
+        public decimal gun_angle { get; set; }
+
+        public Player(string username, Tank tank) 
+        {
+            this.username = username;
+            this.tank = tank;
+            this.ammo = tank.ammo_storage;
+            this.health = tank.max_health;
+            this.gun_angle = 15;
+        }
+        Random random = new Random();
+
+        public void Hit(int potencial_damage)
+        {
+            int damage = potencial_damage - (armor / 4);
+            armor = (armor / 4) - random.Next(0,(armor/6));
+        }
+
+        public delegate void ShotHandler(int potencial_damage, int angle);
+        public event ShotHandler Shot;
+
+        public void Shoot()
+        {
+            ammo--;
+            Shot.Invoke(tank.damage, ((int)gun_angle));
+            gun_angle += random.Next(-5, 5);
+            gun_align();    
+        }
+
+        private void gun_align()
+        {   
+            if (gun_angle < 0) gun_angle = 0;
+            else if (gun_angle > 90) gun_angle = 90;
+        }
+
+
+
+    }
+}
---
+++ Balistic Tank Game/Tank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balistic_Tank_Game
+{
+    class Tank
+    {
+        public string name { get; internal set; }
+        public string description { get; internal set; }
+        public int ammo_storage {  get; internal set; }
+        public int max_armor { get; internal set; }
+        public int max_health { get; internal set; }
+        public int damage { get; internal set; }
+
+        public Tank(string name, string description, int ammo_storage, int max_armor, int max_health, int damage)
+        {
+            this.name = name;
+            this.description = description;
+            this.ammo_storage = ammo_storage;
+            this.max_armor = max_armor;
+            this.max_health = max_health;
+            this.damage = damage;
+        }
+    }
+}
